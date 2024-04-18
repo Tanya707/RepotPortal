25c1fa87d2964c906a623a255e8d191108605857
Tatsiana Barbitskaya <Tatsiana_Barbitskaya@epam.com>
2024-04-18 17:04:18 +0300

    Update private

--- Core/Helpers/Waiter.cs
+++ Core/Helpers/Waiter.cs
@@ -5,19 +5,19 @@ namespace Core.Helpers
 {
     public class Waiter
     {
-        private IWebDriver Driver;
-        private WebDriverWait wait;
+        private IWebDriver _driver;
+        private WebDriverWait _wait;
         private static readonly TimeSpan ConditionTimeOutDefault = TimeSpan.FromSeconds(10);
 
         public Waiter(IWebDriver driver)
         {
-            Driver = driver;
+            _driver = driver;
         }
 
         public void WaitFor(Func<bool> condition, TimeSpan conditionTimeOut = default)
         {
-            wait = new WebDriverWait(Driver, conditionTimeOut == default(TimeSpan) ? ConditionTimeOutDefault : conditionTimeOut);
-            wait.Until(_ => condition.Invoke());
+            _wait = new WebDriverWait(_driver, conditionTimeOut == default(TimeSpan) ? ConditionTimeOutDefault : conditionTimeOut);
+            _wait.Until(_ => condition.Invoke());
         }
     }
 }
